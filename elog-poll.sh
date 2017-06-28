64c330b8f9b0178c938d5d8df2c84630adf53bc8
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2017-06-28 16:30:35 +0200

    first commit
    the script is briefly documented in the comment on the top.
    
    The present implementation is able to check the presence of the
    configuration and run the elog client iterating over a number of posts
    - from the one whose ID is stored on file to the last inserted - or only
    on one.
    
    The handler function is still missing implementation.

---
+++ elog-poll.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+#
+# elog-poll
+#
+# This script repeatedly checks whether new elog posts have been added
+# to a specific ELOG logbook and eventually executes a program to handle
+# the post.
+#
+# Author: Carmelo Pellegrino <carmelo.pellegrino@cnaf.infn.it>
+#
+
+# The configuration of this very script is perfomed via shell variables.
+# Here the list of parameters is reported:
+#
+# - ELOG_EXE       -> Path to the 'elog' executable
+# - ELOG_SERVER    -> Address of the ELOG server
+# - ELOG_PORT      -> Port of the ELOG server
+# - ELOG_USES_SSL  -> Specify the use of SSL for connecting to ELOG [y|n]
+# - ELOG_BOOK      -> Logbook to poll
+# - ELOG_USER      -> User name for reading ELOG posts
+# - ELOG_PASSWORD  -> Password of the user specified in the variable above
+# - ELOG_LAST_POST -> Path of the file containing the id of the first post
+#                     to check
+#
+
+# The user may want to set the configuration variables here
+#ELOG_EXE=
+#ELOG_SERVER=
+#ELOG_PORT=
+#ELOG_USES_SSL=
+#ELOG_BOOK=
+#ELOG_USER=
+#ELOG_PASSWORD=
+#ELOG_LAST_POST=
+
+function show_help() {
+  echo "Usage:"
+  echo "  $(basename "$0") [id]"
+  echo "if the id parameter is specified, the program checks that ID only,"
+  echo "otherwise it checks all the IDs starting from that stored in the"
+  echo "${ELOG_LAST_POST} file."
+}
+
+function check_config() {
+  local missing=()
+  [ -n "${ELOG_EXE+x}"       ] || missing+=(ELOG_EXE)
+  [ -n "${ELOG_SERVER+x}"    ] || missing+=(ELOG_SERVER)
+  [ -n "${ELOG_PORT+x}"      ] || missing+=(ELOG_PORT)
+  [ -n "${ELOG_USES_SSL+x}"  ] || missing+=(ELOG_USES_SSL)
+  [ -n "${ELOG_BOOK+x}"      ] || missing+=(ELOG_BOOK)
+  [ -n "${ELOG_USER+x}"      ] || missing+=(ELOG_USER)
+  [ -n "${ELOG_PASSWORD+x}"  ] || missing+=(ELOG_PASSWORD)
+  [ -n "${ELOG_LAST_POST+x}" ] || missing+=(ELOG_LAST_POST)
+
+  if [ ! ${#missing} -eq 0 ]; then
+    echo "ERROR: the following environment variables are missing:" >&2
+    for miss in ${missing[*]}; do
+      echo "${miss}" >&2
+    done
+    echo "Please set them before running again this program." >&2
+
+    return 1
+  fi
+
+  return 0
+}
+
+function get_last_post_id() {
+  [ ! -e "${ELOG_LAST_POST}" ] && echo 1 > "${ELOG_LAST_POST}"
+  cat "${ELOG_LAST_POST}"
+}
+
+function set_last_post_id() {
+  echo "$1" >"${ELOG_LAST_POST}"
+}
+
+function get_post() {
+  local ID=$1
+  local ssl=""
+  [ "${ELOG_USES_SSL}" == "y" ] && ssl="-s"
+
+  ${ELOG_EXE}                             \
+    -w "${ID}" -l "${ELOG_BOOK}" "${ssl}" \
+    -h "${ELOG_SERVER}" -p "${ELOG_PORT}" \
+    -u "${ELOG_USER}" "${ELOG_PASSWORD}"
+}
+
+function handler() {
+  echo "Implement me!"
+}
+
+# Script begins here
+check_config || exit 1
+
+if [ -n "$1" ]; then
+  if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    show_help
+  else
+    # Handle a specific ELOG ID
+    get_post "$1" | handler
+  fi
+else
+  # Handle the whole archive from the last checked
+  ID=$(get_last_post_id)
+  while true; do
+    get_post "${ID}" | handler || break
+    ((++ID))
+  done
+  set_last_post_id $ID
+fi
+
