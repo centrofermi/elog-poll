e5b87d2f3ed4308beecc1af9bc7d74f9fa74b0b6
Francesco Noferini <noferini@bo.infn.it>
2017-09-29 11:21:07 +0200

    fix for only dst files

--- src/producer.cpp
+++ src/producer.cpp
@@ -106,7 +106,7 @@ int main(int argc, char** argv)
       << pathToRecon << '/'
       << school << '/'
       << currentday << '/'
-      << schoolInFile << "*.root";
+      << schoolInFile << "*dst.root";
 
     nfile += chain.Add(oss.str().c_str());
 
