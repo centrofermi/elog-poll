41994089eca9218f87f9d4eb0b88d5ab270dbdf2
Carmelo Pellegrino <carmelo.pellegrino@gmail.com>
2018-05-04 11:12:21 +0200

    directory listing and filtering through regex library implemented
    waiting for C++17 std::filesystem library, the dirent.h is used.
     - A custom "directory" type is implemented
     - A set of functions operating over the directory object are also provided:
        * open_dir(path): returns a directory object from the "path"
          object (C-like or std:: string);
        * ls(directory): returns an std::vector containing directory item names
          as std::strings;
        * matching_items(directory, pattern): returns an std::vector containing
          directory item names matching the given pattern with regex syntax, e.g.:
          "*.bin" -> ".*\.bin"
     - A small test file has also been added.

---
+++ include/directory.hpp
@@ -0,0 +1,71 @@
+#ifndef DIRECTORY_HPP
+#define DIRECTORY_HPP
+
+#include <memory>
+#include <algorithm>
+#include <regex>
+#include <stdexcept>
+#include <cerrno>
+#include <dirent.h>
+
+auto dircloser = [](DIR* dir) { return closedir(dir); };
+using directory = std::unique_ptr<DIR, decltype(dircloser)>;
+
+inline
+directory open_dir(std::string const& path)
+{
+  auto dir = opendir(path.c_str());
+
+  if (dir == nullptr) {
+    auto const e = errno;
+    throw std::runtime_error(strerror(e));
+  }
+
+  return directory(dir, dircloser);
+}
+
+inline
+std::vector<std::string> ls(directory& dir)
+{
+  rewinddir(dir.get());
+  dirent const* element = nullptr;
+
+  std::vector<std::string> ret;
+
+  while ((element = readdir(dir.get()))) {
+    ret.push_back(element->d_name);
+  }
+
+  return ret;
+}
+
+//  auto begin = std::sregex_iterator(
+//      std::begin(list)
+//    , std::end(list)
+//    , regex
+//  );
+//
+//  auto const end = std::sregex_iterator();
+
+inline
+std::vector<std::string> matching_items(directory& dir, std::string pattern)
+{
+  auto const list = ls(dir);
+
+  std::regex const regex { pattern };
+
+  std::vector<std::string> ret;
+
+  std::copy_if(
+      begin(list)
+    , end(list)
+    , std::back_inserter(ret)
+    , [&regex](std::string const& item) {
+        return std::regex_match(std::begin(item), std::end(item), regex);
+      }
+  );
+
+  return ret;
+}
+
+#endif // DIRECTORY_HPP
---
+++ test/test_directory.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <directory.hpp>
+
+#define throw_assert(assertion, message) do {\
+    if (!(assertion)) throw std::runtime_error("condition \`" #assertion "` failed: " message); \
+} while (0);
+
+int main()
+{
+  try {
+    // try opening a non existing folder -- this shall fail
+    try {
+      open_dir("/nonexisting");
+
+      // the following lines shall not execute
+      std::cerr << "ERROR: /nonexisting folder correcly open!\n";
+      return EXIT_FAILURE;
+    } catch (...) {}
+
+    // this part shall not fail
+
+    auto bin = open_dir("/bin/");
+
+    auto const list = ls(bin);
+
+    throw_assert(!list.empty(), "empty /bin dir");
+
+    auto const shells = matching_items(bin, ".*sh");
+
+    throw_assert(!shells.empty(), "no shell programs in /bin dir");
+  } catch (std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+}
